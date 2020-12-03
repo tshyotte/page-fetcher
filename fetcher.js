6c7f92a227c93a238526612ce1fcfac3fabeaf38
Odette Bahome <odettebahome@gmail.com>
2020-12-03 16:40:16 +0000

    download file

---
+++ fetcher.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+const url = process.argv.slice(2)[0]; // link to file you want to download
+const path = process.argv.slice(2)[1]; // where to save a file
+
+const request = http.get(url, function(response) {
+    if (response.statusCode === 200) {
+        var file = fs.createWriteStream(path);
+        response.pipe(file);
+    }
+    request.setTimeout(60000, function() { // if after 60s 
+        request.abort();
+    });
+});
+
